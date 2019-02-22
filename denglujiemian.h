3e7ff8d2be85741d48981fa5dd397394df064cc7
xiaokekuuga <47215645+xiaokekuuga@users.noreply.github.com>
2019-02-22 17:41:25 +0800

    Add files via upload
    头文件.h

---
+++ denglujiemian.h
@@ -0,0 +1,29 @@
+#ifndef DENGLUJIEMIAN_H
+#define DENGLUJIEMIAN_H
+#include <iostream>
+using namespace std;
+
+class denglu
+{
+	public:
+		void denglujiemian();
+		int password;
+		int key;
+		int num;
+	    int mun; 
+	    int password1;
+	    int password2;
+	    int password3;
+	    int password4;
+	    int key0;
+	    int key1;
+	    int key2;
+	    int key3;
+	    int key4;
+	    int can1;
+	//  protected:   如果不需要protected变量请删掉 protected： 
+};   // } 在类的结尾需要 ；！！！ 
+		
+
+#endif
+
---
+++ xkchange.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <cstring> 
+#include <time.h>
+#include <conio.h> 
+using namespace std; 
+void Order_();/*进入食物分类主界面选择*/
+int Order_hamburger();/*汉堡界面*/
+int Order_snack();/*小吃界面*/
+int Order_beverage();/*饮料界面*/
+void Order_();/*进入食物分类主界面选择*/
+void getFood();/*录入食物数据*/
+void Order_change();/*中间选项*/ 
+int jiezhan(); /*结账 */  
+void jiesu();/*跳转到结束界面*/
+
+
