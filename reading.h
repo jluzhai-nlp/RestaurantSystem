5fd3308ae42a8e8b1863032adb9a0b91fa54711f
xiaokekuuga <47215645+xiaokekuuga@users.noreply.github.com>
2019-02-22 17:32:25 +0800

    Delete reading.h

--- reading.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstring>
-#include <stdlib.h>
-#include <fstream> 
-using namespace std; 
-double reading_price(int x);/*从txt中读取商品价格*/
-string reading_name(int x);/*从txt中读取商品名称*/ 
-void change_();/*跳转到选项界面*/
-
